6580b553f91969195ddb0374045e935861d3af80
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2012-07-17 23:03:47 -0300

    just make some cleaning code

--- Library/Pop3Client.cs
+++ Library/Pop3Client.cs
@@ -26,73 +26,66 @@ namespace Pop3
         private string _server;
         private int _portNumber = 995;
         private bool _useSsl = true;
-        private bool _isConnected = false;
+        private bool _isConnected;
 
         private Stream _pop3Stream;
-        
-        #endregion
-
-        #region Constructors
-
-        public Pop3Client( ) { }
 
         #endregion
+
 
         #region Public Methods
 
-        public void Connect( string server, string userName, string password )
+        public void Connect(string server, string userName, string password)
         {
-            Connect( server, userName, password, 110, false );    
+            Connect(server, userName, password, 110, false);
         }
 
-        public void Connect( string server, string userName, string password, bool useSsl )
+        public void Connect(string server, string userName, string password, bool useSsl)
         {
-            Connect( server, userName, password, (useSsl ? 995 : 110), useSsl);
+            Connect(server, userName, password, (useSsl ? 995 : 110), useSsl);
         }
-        
-        public void Connect( string server, string userName, string password, int portNumber, bool useSsl )
+
+        public void Connect(string server, string userName, string password, int portNumber, bool useSsl)
         {
-            if ( _isConnected )
-                throw new Pop3Exception( "Pop3 client already connected" );
+            if (_isConnected)
+                throw new Pop3Exception("Pop3 client already connected");
 
             _server = server;
             _useSsl = useSsl;
             _portNumber = portNumber;
 
-            Connect( server, _portNumber );
+            Connect(server, _portNumber);
 
-            if ( _useSsl )
+            if (_useSsl)
             {
-                _pop3Stream = new SslStream( GetStream( ), false );
-                ( (SslStream)_pop3Stream ).AuthenticateAsClient( _server );
+                _pop3Stream = new SslStream(GetStream(), false);
+                ((SslStream)_pop3Stream).AuthenticateAsClient(_server);
             }
             else
             {
-                _pop3Stream = GetStream( );
+                _pop3Stream = GetStream();
             }
 
-            string response = Response( );
-            if ( response.Substring( 0, 3 ) != "+OK" )
+            var response = Response();
+            if (response.Substring(0, 3) != "+OK")
             {
-                throw new Pop3Exception( response );
+                throw new Pop3Exception(response);
             }
-            else
-            {
-                SendCommand( String.Format( CultureInfo.InvariantCulture, "USER {0}", userName ) );
-                SendCommand( String.Format( CultureInfo.InvariantCulture, "PASS {0}", password ) );
-            }
+
+            SendCommand(String.Format(CultureInfo.InvariantCulture, "USER {0}", userName));
+            SendCommand(String.Format(CultureInfo.InvariantCulture, "PASS {0}", password));
 
             _isConnected = true;
         }
 
-        public void Disconnect( )
+        public void Disconnect()
         {
-            if ( !_isConnected )
+            if (!_isConnected)
                 return;
 
             try
             {
-                SendCommand( "QUIT" );
+                SendCommand("QUIT");
             }
             finally
             {
@@ -100,177 +93,169 @@ namespace Pop3
             }
         }
 
-        public List<Pop3Message> List( )
+        public List<Pop3Message> List()
         {
-            string response;
+            var result = new List<Pop3Message>();
 
-            List<Pop3Message> result = new List<Pop3Message>( );
+            SendCommand("LIST");
 
-            SendCommand( "LIST" );
-
-            while ( true )
+            while (true)
             {
-                response = Response( );
-                if ( response == ".\r\n" )
-                {
+                var response = Response();
+                if (response == ".\r\n")
                     return result;
-                }
-                else
-                {
-                    Pop3Message message = new Pop3Message( );
-                    
-                    char[ ] seps = { ' ' };
-                    string[ ] values = response.Split( seps );
-                    
-                    message.Number = Int32.Parse( values[ 0 ] );
-                    message.Bytes = Int32.Parse( values[ 1 ] );
-                    message.Retrieved = false;
-                    
-                    result.Add( message );
-                }
+
+
+                var message = new Pop3Message();
+
+                char[] seps = { ' ' };
+                var values = response.Split(seps);
+
+                message.Number = Int32.Parse(values[0]);
+                message.Bytes = Int32.Parse(values[1]);
+                message.Retrieved = false;
+
+                result.Add(message);
             }
         }
 
-        public void RetrieveHeader( Pop3Message message )
+        public void RetrieveHeader(Pop3Message message)
         {
-            string response;
+            SendCommand("TOP", "0", message);
 
-			SendCommand( "TOP", "0", message );
-
-            while ( true )
+            while (true)
             {
-                response = Response( );
-                if ( response == ".\r\n" )
+                var response = Response();
+                if (response == ".\r\n")
                     break;
-                else
-                    message.Header += response;
+
+                message.Header += response;
             }
         }
-        
-        public void Retrieve( Pop3Message message )
-        {
-            string response;
 
-            SendCommand( "RETR", message );
+        public void Retrieve(Pop3Message message)
+        {
+            SendCommand("RETR", message);
 
             message.Retrieved = true;
-            while ( true )
+            while (true)
             {
-                response = Response( );
-                if ( response == ".\r\n" )
+                var response = Response();
+                if (response == ".\r\n")
                     break;
-                else
-                    message.Message += response;
+
+                message.Message += response;
             }
         }
 
-        public void Retrieve( List<Pop3Message> messages )
+        public void Retrieve(List<Pop3Message> messages)
         {
-            foreach ( Pop3Message message in messages )
-                Retrieve( message );
+            foreach (var message in messages)
+            {
+                Retrieve(message);
+            }
         }
 
-        public List<Pop3Message> ListAndRetrieve( )
+        public List<Pop3Message> ListAndRetrieve()
         {
-            List<Pop3Message> messages = List( );
+            var messages = List();
 
-            Retrieve( messages );
+            Retrieve(messages);
 
             return messages;
         }
-        
-        public void Delete( Pop3Message message )
-        {
-            SendCommand( "DELE", message );
-        }
-        
-        public void Dispose( )
-        {
-            Dispose( true );
 
-            GC.SuppressFinalize( this );
+        public void Delete(Pop3Message message)
+        {
+            SendCommand("DELE", message);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
         }
 
         #endregion
 
+
         #region Private Methods
 
-        protected override void Dispose( bool disposing )
+        protected override void Dispose(bool disposing)
         {
-            Disconnect( );
+            Disconnect();
 
-            base.Dispose( disposing );
+            base.Dispose(disposing);
         }
 
-        private void SendCommand( string command )
+        private void SendCommand(string command, Pop3Message message)
         {
-            SendCommand( command, null, null );
+            SendCommand(command, null, message);
         }
 
-        private void SendCommand( string command, Pop3Message message )
+        private void SendCommand(string command, string aditionalParameters = null, Pop3Message message = null)
         {
-			SendCommand( command, null, message );
-		}
+            var request = new StringBuilder();
 
-		private void SendCommand( string command, string aditionalParameters, Pop3Message message )
-		{
-			StringBuilder request = new StringBuilder();
-			string response;
-
-			if ( message == null )
-				request.AppendFormat( CultureInfo.InvariantCulture, "{0}", command );
-			else
-				request.AppendFormat( CultureInfo.InvariantCulture, "{0} {1}", command, message.Number );
-
-			if ( !String.IsNullOrEmpty( aditionalParameters ))
-				request.AppendFormat( " {0}", aditionalParameters );
-				
-			request.Append( "\r\n" );
-			
-			Write( request.ToString( ) );
-
-			response = Response( );
-			if ( response.Substring( 0, 3 ) != "+OK" )
-				throw new Pop3Exception( response );
-		}
-		
-		private void Write( string message )
-        {
-            ASCIIEncoding en = new ASCIIEncoding( );
-
-            byte[ ] writeBuffer = new byte[ 1024 ];
-            writeBuffer = en.GetBytes( message );
-
-            _pop3Stream.Write( writeBuffer, 0, writeBuffer.Length );
-    }
-
-        private string Response( )
-        {
-            ASCIIEncoding enc = new ASCIIEncoding( );
-
-            byte[ ] serverBuffer = new Byte[ 1024 ];
-
-            int count = 0;
-
-            while ( true )
+            if (message == null)
             {
-                byte[ ] buff = new Byte[ 2 ];
-                int bytes = _pop3Stream.Read( buff, 0, 1 );
-                if ( bytes == 1 )
-                {
-                    serverBuffer[ count ] = buff[ 0 ];
-                    count++;
+                request.AppendFormat(CultureInfo.InvariantCulture, "{0}", command);
+            }
+            else
+            {
+                request.AppendFormat(CultureInfo.InvariantCulture, "{0} {1}", command, message.Number);
+            }
 
-                    if ( buff[ 0 ] == '\n' )
-                        break;
-                }
-                else
+            if (!String.IsNullOrEmpty(aditionalParameters))
+            {
+                request.AppendFormat(" {0}", aditionalParameters);
+            }
+
+            request.Append("\r\n");
+
+            Write(request.ToString());
+
+            var response = Response();
+
+            if (response.Substring(0, 3) != "+OK")
+                throw new Pop3Exception(response);
+        }
+
+        private void Write(string message)
+        {
+            var en = new ASCIIEncoding();
+
+            var writeBuffer = en.GetBytes(message);
+
+            _pop3Stream.Write(writeBuffer, 0, writeBuffer.Length);
+        }
+
+        private string Response()
+        {
+            var enc = new ASCIIEncoding();
+
+            var serverBuffer = new Byte[1024];
+
+            var count = 0;
+
+            while (true)
+            {
+                var buff = new Byte[2];
+                var bytes = _pop3Stream.Read(buff, 0, 1);
+                if (bytes != 1)
                 {
                     break;
                 }
+                
+                serverBuffer[count] = buff[0];
+                count++;
+
+                if (buff[0] == '\n')
+                    break;
             }
 
-            return enc.GetString( serverBuffer, 0, count );
+            return enc.GetString(serverBuffer, 0, count);
         }
 
         #endregion
