ed4eb0b7503330723341a03704d4453a4127642e
Rodolfo Finochietti <rfinochi@gmail.com>
2013-01-18 15:53:19 -0300

    Changes in compilation script for support WinRT version closes #9

--- Resources/Misc/GlobalAssemblyInfo.cs
+++ Resources/Misc/GlobalAssemblyInfo.cs
@@ -20,9 +20,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCulture( "" )]
 
-[assembly: AssemblyVersion( "1.5.0.1" )]
-[assembly: AssemblyFileVersion( "1.5.0.1" )]
-[assembly: AssemblyInformationalVersion( "1.5.0.1" )]
+[assembly: AssemblyVersion( "2.0.0.1" )]
+[assembly: AssemblyFileVersion( "2.0.0.1" )]
+[assembly: AssemblyInformationalVersion( "2.0.0.1" )]
 
 [assembly: ComVisible( false )]
 [assembly: CLSCompliant( true )]
