99231dce320a3474c2aee1e14cae01d5430834d8
Rodolfo Finochietti <rfinochi@gmail.com>
2019-11-14 11:08:54 -0300

    Bug Fix: The disconnect method was not called on the object disposing

--- Resources/Misc/GlobalAssemblyInfo.cs
+++ Resources/Misc/GlobalAssemblyInfo.cs
@@ -18,9 +18,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCulture( "" )]
 
-[assembly: AssemblyVersion( "2.3.1" )]
-[assembly: AssemblyFileVersion( "2.3.1" )]
-[assembly: AssemblyInformationalVersion( "2.3.1" )]
+[assembly: AssemblyVersion( "2.3.2" )]
+[assembly: AssemblyFileVersion( "2.3.2" )]
+[assembly: AssemblyInformationalVersion( "2.3.2" )]
 
 [assembly: ComVisible( false )]
 
